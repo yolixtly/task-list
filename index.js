ab91b146e023a3f4357ab043e1e3c67522731e23
Chris Klanac <cklanac@gmail.com>
2016-09-02 08:47:18 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var HelloWorld = function() {
+    return (
+        <div>Hello World</div>
+    );
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    ReactDOM.render(<HelloWorld />, document.getElementById('app'));
+});
