11e899a6855c2780628fe94ecba60faf37d972c3
Casey Olson <casey@bookcasey.com>
2016-09-07 19:14:28 -0500

    clean up

--- bs-config.js
+++
@@ -1,9 +0,0 @@
-/* global module */
-/* eslint no-undef: "error" */
-
-//https://scotch.io/bar-talk/a-fast-and-convenient-development-server-with-lite-server
-
-module.exports = {
-    "browser": "google chrome",
-    "server" : { "baseDir" : "./build" }
-};
--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use('/hello', express.static('build'));
+app.use('/', express.static('build'));
 
 app.listen(3000, function () {
   console.log('Listening at 3000!');
